0ee04c510a8e0ed3228fc32771bc54f76862f2ec
Andrei <andreigabor36@gmail.com>
2021-05-25 10:53:57 +0300

    Added presentation

---
+++ golang_api/controller/book-controller.go
@@ -0,0 +1,141 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/andreigabor21/golang_api/dto"
+	"github.com/andreigabor21/golang_api/entity"
+	"github.com/andreigabor21/golang_api/helper"
+	"github.com/andreigabor21/golang_api/service"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type BookController interface {
+	All(context *gin.Context)
+	FindByID(context *gin.Context)
+	Insert(context *gin.Context)
+	Update(context *gin.Context)
+	Delete(context *gin.Context)
+}
+
+type bookController struct {
+	bookService service.BookService
+	jwtService service.JWTService
+}
+
+func NewBookController(bookService service.BookService, jwtService service.JWTService) BookController {
+	return &bookController{
+		bookService: bookService,
+		jwtService: jwtService,
+	}
+}
+
+func (c *bookController) All(context *gin.Context) {
+	var books []entity.Book = c.bookService.All()
+	res := helper.BuildResponse(true, "OK", books)
+	context.JSON(http.StatusOK, res)
+}
+
+func (c *bookController) FindByID(context *gin.Context) {
+	id, err := strconv.ParseUint(context.Param("id"), 0, 0)
+	if err != nil {
+		res := helper.BuildErrorResponse("No param id", err.Error(), helper.EmptyObj{})
+		context.AbortWithStatusJSON(http.StatusBadRequest, res)
+		return
+	}
+	var book entity.Book = c.bookService.FindById(id)
+	if (book == entity.Book{}) {
+		res := helper.BuildErrorResponse("Data not found", "No data with given id", helper.EmptyObj{})
+		context.AbortWithStatusJSON(http.StatusBadRequest, res)
+		return
+	} else {
+		res := helper.BuildResponse(true, "OK", book)
+		context.JSON(http.StatusOK, res)
+	}
+}
+
+func (c *bookController) Insert(context *gin.Context) {
+	var bookCreateDTO dto.BookCreateDTO
+	errDTO := context.ShouldBind(&bookCreateDTO)
+	if errDTO != nil {
+		res := helper.BuildErrorResponse("Failed to process request", errDTO.Error(), helper.EmptyObj{})
+		context.JSON(http.StatusBadRequest, res)
+	} else {
+		authHeader := context.GetHeader("Authorization")
+		userID := c.getUserIdByToken(authHeader)
+		convertedUserId, err := strconv.ParseUint(userID, 10, 64)
+		if err == nil {
+			bookCreateDTO.UserID = convertedUserId
+		}
+		result := c.bookService.Insert(bookCreateDTO)
+		response := helper.BuildResponse(true, "OK", result)
+		context.JSON(http.StatusOK, response)
+	}
+}
+
+func (c *bookController) Update(context *gin.Context) {
+	var bookUpdateDTO dto.BookUpdateDTO
+	errDTO := context.ShouldBind(&bookUpdateDTO)
+	if errDTO != nil {
+		res := helper.BuildErrorResponse("Failed to process request", errDTO.Error(), helper.EmptyObj{})
+		context.JSON(http.StatusBadRequest, res)
+		return
+	}
+
+	authHeader := context.GetHeader("Authorization")
+	token, errToken := c.jwtService.ValidateToken(authHeader)
+	if errToken != nil {
+		panic(errToken.Error())
+	}
+	claims := token.Claims.(jwt.MapClaims)
+	userID := fmt.Sprintf("%v", claims["user_id"])
+	if c.bookService.IsAllowedToEdit(userID, bookUpdateDTO.ID) {
+		id, errID := strconv.ParseUint(userID, 10, 64)
+		if errID == nil {
+			bookUpdateDTO.UserID = id
+		}
+		result := c.bookService.Update(bookUpdateDTO)
+		response := helper.BuildResponse(true, "OK", result)
+		context.JSON(http.StatusOK, response)
+	} else {
+		response := helper.BuildErrorResponse("You dont have permission", "You are not the owner", helper.EmptyObj{})
+		context.JSON(http.StatusForbidden, response)
+	}
+}
+
+func (c *bookController) Delete(context *gin.Context) {
+	var book entity.Book
+	id, err := strconv.ParseUint(context.Param("id"), 0, 0)
+	if err != nil {
+		response := helper.BuildErrorResponse("Failed to get id", "No param id found", helper.EmptyObj{})
+		context.JSON(http.StatusBadRequest, response)
+	}
+	book.ID = id
+	authHeader := context.GetHeader("Authorization")
+	token, errToken := c.jwtService.ValidateToken(authHeader)
+	if errToken != nil {
+		panic(errToken.Error())
+	}
+	claims := token.Claims.(jwt.MapClaims)
+	userID := fmt.Sprintf("%v", claims["user_id"])
+	if c.bookService.IsAllowedToEdit(userID, book.ID) {
+		c.bookService.Delete(book)
+		res := helper.BuildResponse(true, "Deleted", helper.EmptyObj{})
+		context.JSON(http.StatusOK, res)
+	} else {
+		response := helper.BuildErrorResponse("You do not have permission", "Not the owner", helper.EmptyObj{})
+		context.JSON(http.StatusForbidden, response)
+	}
+}
+
+func (c *bookController) getUserIdByToken(token string) string {
+	aToken, err := c.jwtService.ValidateToken(token)
+	if err != nil {
+		panic(err.Error())
+	}
+	claims := aToken.Claims.(jwt.MapClaims)
+	id := fmt.Sprintf("%v", claims["user_id"])
+	return id
+}
---
+++ golang_api/go.mod
@@ -0,0 +1,13 @@
+module github.com/andreigabor21/golang_api
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.7.1 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/mashingan/smapping v0.1.9 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	gorm.io/driver/mysql v1.1.0 // indirect
+	gorm.io/gorm v1.21.10 // indirect
+)
